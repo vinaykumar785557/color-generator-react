0d58b58611bb521e7f3eafa4d2de73de93c55410
vinaykumar785557 <c0785557@mylambton.ca>
2022-03-19 21:28:05 -0400

    added values.js values and handling form submission

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { useState } from 'react';
 import classes from './App.module.css';
 
+import Values from 'values.js';
+
 function App() {
 	const [color, setColor] = useState('');
 
 	const [error, setError] = useState(false);
 
+	const [list, setList] = useState(new Values('#FF7777').all(10));
+
+	// console.log(list);
 	const changeHandler = (e) => {
 		setColor(e.target.value);
 	};
 
 	const submitHandler = (e) => {
 		e.preventDefault();
-		alert('submit button clicked');
+
+		try {
+			let colors = new Values(color).all(10);
+			setList(colors);
+		} catch (error) {
+			setError(true);
+			console.log(error);
+		}
 	};
 
 	return (
@@ -34,7 +46,15 @@ function App() {
 					</button>
 				</form>
 			</section>
-			<p>{color}</p>
+      
+      <section>
+        
+      </section>
+
+
+
+
+
 		</>
 	);
 }

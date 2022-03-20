cc5d662daabe519c25983f714ab42dcc9e272a3a
vinaykumar785557 <c0785557@mylambton.ca>
2022-03-19 21:12:20 -0400

    search input and button added and also styled using classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import './App.css';
+import { useState } from 'react';
+import classes from './App.module.css';
 
 function App() {
-	return <div className='App'>Color Generator App</div>;
+	const [color, setColor] = useState('');
+
+	const [error, setError] = useState(false);
+
+	const changeHandler = (e) => {
+		setColor(e.target.value);
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		alert('submit button clicked');
+	};
+
+	return (
+		<>
+			<section className={classes.container}>
+				<h3>Color Generator</h3>
+
+				<form onSubmit={submitHandler}>
+					<input
+						type='text'
+						placeholder='type color'
+						value={color}
+						onChange={changeHandler}
+						className={`${
+							error ? classes.input_error : classes.input
+						}`}></input>
+					<button type='submit' className={classes.btn}>
+						Get Colors
+					</button>
+				</form>
+			</section>
+			<p>{color}</p>
+		</>
+	);
 }
 
 export default App;
